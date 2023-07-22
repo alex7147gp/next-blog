93b0d9f46a61f34d4d402ae3f0353a5c64c9b1f2
alex7147gp <johncedillo77@gmail.com>
2023-07-22 16:46:14 -0500

    [first commit]

---
+++ src/site-meta.config.ts
@@ -0,0 +1,12 @@
+const siteMeta = {
+	title: "@JohnC",
+	description:
+		"Learning Engineer at @Center - Lead at Developer, #JavaScript #React #Web3 #BTC #ETH - #Twitch Partner 🇲🇽 🇨🇴",
+	lang: "es-ES",
+	ogLocale: "es_ES",
+	githubUrl: "https://github.com/alex7147gp",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
